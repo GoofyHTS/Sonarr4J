48191b1d9c726ad505fc179b775d07317ba1cb0a
GoofyHTS <goofyhts@gmail.com>
2016-07-04 22:43:59 +0200

    added sonarr api creation test

--- src/main/java/goofyhts/sonarr4j/rest/api/SonarrApi.java
+++ src/main/java/goofyhts/sonarr4j/rest/api/SonarrApi.java
@@ -4,6 +4,28 @@ package goofyhts.sonarr4j.rest.api;
  * Created by Goofy on 2016/07/04.
  */
 public class SonarrApi {
+
+    private String baseUrl;
+    private String apiKey;
+
     public SonarrApi(String baseUrl, String apiKey) {
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
     }
 }
--- src/test/java/goofyhts/sonarr4j/rest/api/SonarrApiTest.java
+++ src/test/java/goofyhts/sonarr4j/rest/api/SonarrApiTest.java
@@ -1,6 +1,7 @@
 package goofyhts.sonarr4j.rest.api;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Goofy on 2016/07/04.
@@ -8,8 +9,13 @@ import org.junit.Test;
 public class SonarrApiTest {
 
     @Test
-    public void name() throws Exception {
-        SonarrApi sonarrApi = new SonarrApi("http://url.com", "ApiKey");
+    public void sonarrApiTest() throws Exception {
+        String baseUrl = "http://base.url.com";
+        String apiKey = "apiKey";
 
+        SonarrApi sonarrApi = new SonarrApi(baseUrl, apiKey);
+
+        assertEquals(baseUrl, sonarrApi.getBaseUrl());
+        assertEquals(apiKey, sonarrApi.getApiKey());
     }
 }
